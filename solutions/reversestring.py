a1f3099f3d17bff47a09bca5548a9afcc7998291
Kimfucious <kim@abts.io>
2018-08-16 03:29:23 +0700

    Update reversestring

--- solutions/reversestring.py
+++ solutions/reversestring.py
@@ -7,5 +7,27 @@
 #    reverse('Greetings!') === '!sgniteerG'
 
 
-def reverse(str):
-    return str[:: -1]
+def reverse(string):
+    """
+    Using a slice with a stride of -1 to reverse the string directly 
+    """
+    return string[:: -1]
+
+# def reverse(string):
+#     """
+#     Using reduce with a lambda right on the string
+#     No need to split into an array in Python
+#     But you gotta import reduce from functools to use it
+#     """
+#     from functools import reduce
+#     return reduce((lambda acc, curr: curr+acc), string)
+
+
+# def reverse(string):
+#     """
+#     Using a loop
+#     """
+#     reversed_string = ""
+#     for char in string:
+#         reversed_string = char + reversed_string
+#     return reversed_string
