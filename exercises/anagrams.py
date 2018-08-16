f7d25ab56a93b066bf480f57477de839415b58b0
Kimfucious <kim@abts.io>
2018-08-16 15:24:04 +0700

    complete anagrams and fib

--- exercises/anagrams.py
+++ exercises/anagrams.py
@@ -8,10 +8,36 @@
 #   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 #   anagrams('Hi there', 'Bye there') --> False
 
-import re
-
 
 def anagrams(stringA, stringB):
+    """
+    Using regular expression sub() to remove non characters from string
+    with helper function. sorted() returns an array, but no need to join
+    """
+    import re
+
     def clean_string(string):
-        return "".join(sorted(re.sub(r"[^\w]", "", string).lower()))
+        return sorted(re.sub(r"[^\w]", "", string).lower())
     return clean_string(stringA) == clean_string(stringB)
+
+
+# def anagrams(stringA, stringB):
+#     """
+#     Using sorted() directly on the strings w/o helper function
+#     """
+#     import re
+#     return sorted(re.sub(r"[^\w]", "", stringA).lower()) == sorted(re.sub(r"[^\w]", "", stringB).lower())
+
+
+# def anagrams(stringA, stringB):
+#     """
+#     Using dict comprehension to built character maps
+#     with sorted() on the dictionaries
+#     """
+#     import re
+#     char_map_one = {char: stringA.count(
+#         char) for char in re.sub(r"[^\w]", "", stringA).lower()}
+#     char_map_two = {char: stringB.count(
+#         char) for char in re.sub(r"[^\w]", "", stringB).lower()}
+
+#     return sorted(char_map_one) == sorted(char_map_two)
--- tests/test_fib.py
+++ tests/test_fib.py
@@ -2,40 +2,31 @@ import unittest
 from exercises.fib import fib
 
 
-@unittest.skip("skip fib tests")
+# @unittest.skip("skip fib tests")
 class FibonacciTests(unittest.TestCase):
     def test_fib_exists(self):
         self.assertTrue(callable(fib))
 
-    def test_fib_1(self):
-        self.assertEqual(
-            fib(1),
-            1
-        )
+    def test_fib_1(self): self.assertEqual(fib(1), 1)
 
     def test_fib_2(self):
-        self.assertEqual(
-            fib(2),
-            1
-        )
+        self.assertEqual(fib(2), 1)
 
     def test_fib_3(self):
-        self.assertEqual(
-            fib(3),
-            2
-        )
+        self.assertEqual(fib(3), 2)
 
     def test_fib_4(self):
-        self.assertEqual(
-            fib(4),
-            3
-        )
+        self.assertEqual(fib(4), 3)
 
     def test_fib_15(self):
-        self.assertEqual(
-            fib(15),
-            63245986
-        )
+        self.assertEqual(fib(15), 610)
+
+    @unittest.skip("skip fibonacci at 39, because it takes a long time")
+    def test_fib_39(self):
+        """
+        Run this if you want, but know that it can take some time to finish!
+        """
+        self.assertEqual(fib(39), 63245986)
 
 
 if __name__ == "__main__":
