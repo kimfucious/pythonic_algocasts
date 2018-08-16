aa90aafe1bf5d0414a4d0385a7c646c0215bd18d
Kimfucious <kim@abts.io>
2018-08-16 15:48:02 +0700

    complete capitalize

--- solutions/capitalize.py
+++ solutions/capitalize.py
@@ -8,5 +8,29 @@
 #   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 
-def capitalize(str1):
-    return str1.title()
+def capitalize(string):
+    """
+    Using the Python str.title() method
+    """
+    return string.title()
+
+# def capitalize(string):
+#     """
+#     Using a loop on the string
+#     """
+#     result = string[0].upper()
+#     for index in range(1, len(string)):
+#         if string[index-1] == " ":
+#             result += string[index].upper()
+#         else:
+#             result += string[index]
+#     return result
+
+# def capitalize(string):
+#     """
+#     Using a loop on a list split from the string
+#     """
+#     words = []
+#     for word in string.split(" "):
+#         words.append(word[0].upper() + word[1:])
+#     return " ".join(words)
